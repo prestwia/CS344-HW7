ad19b96dd0c2edd9f8475519fc7562e1dacbd056
Alex Prestwich <prestwia@oregonstate.edu>
2021-05-30 16:53:00 -0700

    tests that check detection of multiples of 5

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -8,5 +8,10 @@ def test_3():
     assert fb.fizzbuzz(12) == "Fizz"
     assert fb.fizzbuzz(36) == "Fizz"
     assert fb.fizzbuzz(93) == "Fizz"
-    assert fb.fizzbuzz(4) == 4
-    assert fb.fizzbuzz(28) == 28
+
+def test_5():
+    assert fb.fizzbuzz(5) == "Buzz"
+    assert fb.fizzbuzz(35) == "Buzz"
+    assert fb.fizzbuzz(50) == "Buzz"
+    assert fb.fizzbuzz(95) == "Buzz"
+    
