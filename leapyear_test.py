8ca5163b7c661e2f4d36cd0de5d34de3e5036efe
Alex Prestwich <prestwia@oregonstate.edu>
2021-05-30 17:22:07 -0700

    added test to check that year divisible by 100 is false

--- leapyear_test.py
+++ leapyear_test.py
@@ -3,9 +3,15 @@ from leapyear import leapyear
 
 ly = leapyear()
 
-def test_3():
+def test_div_by_4():
     assert ly.leapyear(2004) == True
     assert ly.leapyear(1996) == True
     assert ly.leapyear(1988) == True
     assert ly.leapyear(2016) == True
+
+def test_div_by_100():
+    assert ly.leapyear(2000) == False
+    assert ly.leapyear(1900) == False
+    assert ly.leapyear(1600) == False
+    assert ly.leapyear(2800) == False
     
