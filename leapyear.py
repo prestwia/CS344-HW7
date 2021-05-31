1efde5a6d286922a81f1bfe267b4a253bfcde3bf
Alex Prestwich <prestwia@oregonstate.edu>
2021-05-30 17:23:31 -0700

    added that year divisible by 100 is not leap year

--- leapyear.py
+++ leapyear.py
@@ -1,4 +1,10 @@
 class leapyear:
     def leapyear(self, year):
+        isLeap = False
+
         if year % 4 == 0:
-            return True
+            isLeap = True
+        if year % 100 == 0:
+            isLeap = False
+
+        return isLeap
