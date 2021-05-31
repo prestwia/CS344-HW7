27c761285476f9749a49bea34b9ad82cb5d75989
Alex Prestwich <prestwia@oregonstate.edu>
2021-05-30 17:05:28 -0700

    altered fizzbuzz to print 1-100 and apply fizz, buzz and fizzbuzz when appropriate

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,11 @@
 class fizzbuzz():
-    def fizzbuzz(self, i):
-        if i % 3 == 0 and i % 5 == 0:
-            return "FizzBuzz"
-        elif i % 3 == 0:
-            return "Fizz"
-        elif i % 5 == 0:
-            return "Buzz"
-        else:
-            return i
+    def fizzbuzz(self):
+        for i in range(0, 101):
+            if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
