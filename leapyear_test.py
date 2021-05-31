f72d92fc7c9f9e41f29d4c80cad9291a757304b6
Alex Prestwich <prestwia@oregonstate.edu>
2021-05-30 17:20:03 -0700

    fixed tests

--- leapyear_test.py
+++ leapyear_test.py
@@ -3,8 +3,9 @@ from leapyear import leapyear
 
 ly = leapyear()
 
-def div_by_4():
-    assert ly.leapyear(1996) == True
+def test_3():
     assert ly.leapyear(2004) == True
-    assert ly.leapyear(1998) == True
-    assert ly.leapyear(1904) == True
+    assert ly.leapyear(1996) == True
+    assert ly.leapyear(1988) == True
+    assert ly.leapyear(2016) == True
+    
